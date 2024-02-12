e9739457860799a6983e5ed778b3f329d47e02a6
MrsNataliia <natalyanaumenko76@gmail.com>
2024-02-12 13:52:13 +0100

    HW 3.3

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
+// http://owu.linkpc.net/carsAPI/v1/doc
+//     Реалізувати створення, видалення та оновлення машин
+
 import React from 'react';
+
+import {CarsContainer} from "./components/CarsContainer";
 
 const App = () => {
     return (
         <div>
-            App
+            <CarsContainer/>
         </div>
     );
 };
